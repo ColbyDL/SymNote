334640b82ad0064f36cca9bbfd7e6ea9483973b5
Colby Lalicker <colby.d.lalicker@gmail.com>
2024-10-06 13:05:24 -0400

    added login page using auth0 next step: styling loading and routing with json

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "./components/navbar";
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function RootLayout({ children }) {
   return (
@@ -8,12 +9,14 @@ export default function RootLayout({ children }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1" />
       </head>
-      <body>
-        <Navbar />
-        <main className="bg-scroll">
-          {children}
-        </main>
-      </body>
+      <UserProvider>
+        <body>
+          <Navbar />
+          <main className="bg-scroll">
+            {children}
+          </main>
+        </body>
+      </UserProvider>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,8 @@
+'use client'
 import HeroWelcome from './components/heroWelcome'
+
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 const HomePage = () => {
   return (
     <>
