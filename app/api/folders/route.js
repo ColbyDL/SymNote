c0ca56c7749bdfd2f1898d86ca7f5a68225c615c
Colby Lalicker <colby.d.lalicker@gmail.com>
2024-10-29 20:53:41 -0400

    adding in functionality for mapping in the profile page

--- app/api/folders/route.js
+++ app/api/folders/route.js
@@ -3,13 +3,28 @@ import connectMongoDB from "../../../libs/mongodb";
 import Folder from "../../../models/folder"
 
 
-
 export async function POST(request) {
-    const { name, parentId} = await request.json();
+    const { name, parentId, profileId } = await request.json(); // Include profileId if applicable
     await connectMongoDB();
-    await Folder.create({name, parentId});
-    return NextResponse.json({ message: "Folder created"}, { status: 201 });
+
+    // Create the new folder
+    const newFolder = await Folder.create({ name, parentId, profileId, folders: [], files: [] });
+
+    // If there's a parentId, update the parent folder's `folders` array
+    if (parentId) {
+        const parentFolder = await Folder.findById(parentId);
+        if (parentFolder) {
+            parentFolder.folders.push(newFolder._id);
+            await parentFolder.save();
+        } else {
+            throw new Error("Parent folder not found");
+        }
+    }
+
+    return NextResponse.json({ message: "Folder created", folderId: newFolder._id }, { status: 201 });
 }
+
+
 
 export async function GET() {
     await connectMongoDB();
