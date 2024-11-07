88826531e7c3527f49925bc4325baf33275a510d
Colby Lalicker <colby.d.lalicker@gmail.com>
2024-11-07 04:02:42 -0500

    deletion and exporting (minus latex)

--- app/components/fileTable.jsx
+++ app/components/fileTable.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 import FileDisplay from './fileDisplay'
 
-const fileTable = ({rootFolder}) => {
+const fileTable = ({rootFolder, removeFile}) => {
 
   return (
     <div className="w-full px-16">
@@ -12,12 +12,13 @@ const fileTable = ({rootFolder}) => {
           <tr className="file-table-header">
             <th className="rounded-tl-3xl">File Name</th>
             <th className="">Last Modified</th>
-            <th className="rounded-tr-3xl">Link</th>
+            <th className="">Link</th>
+            <th className="rounded-tr-3xl">Delete</th>
           </tr>
         </thead>
         <tbody className="w-full">
           {rootFolder.files.map(f => (
-            <FileDisplay key={f._id} file={f} />
+            <FileDisplay key={f._id} file={f} onFileDelete={removeFile} />
 
           ))}
         </tbody>
--- app/components/textEditor.jsx
+++ app/components/textEditor.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
-import MathTool from 'editorjs-math';
-import SymbolPicker from './SymbolPicker';
-import Undo from 'editorjs-undo';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquareRootVariable } from '@fortawesome/free-solid-svg-icons';
-import { faX } from '@fortawesome/free-solid-svg-icons';
-import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+import MathTool from "editorjs-math";
+import SymbolPicker from "./SymbolPicker";
+import Undo from "editorjs-undo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareRootVariable } from "@fortawesome/free-solid-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
+import { faFileExport } from "@fortawesome/free-solid-svg-icons";
 
 import { useParams } from "next/navigation";
 
+import { jsPDF } from "jspdf";
+import katex from "katex";
+import html2canvas from "html2canvas";
+import "katex/dist/katex.min.css";
 
 const textEditor = () => {
   // Stores reference to an Editor.js instance
@@ -18,74 +23,71 @@ const textEditor = () => {
   const [isMathMode, setIsMathMode] = useState(false);
   const [currentMathBlockIndex, setCurrentMathBlockIndex] = useState(null);
   const { fileId } = useParams();
-  const [file, setFile] = useState()
-  const [loading, setLoading] = useState(true)
-  const [ fileName, setFileName ] = useState("")
+  const [file, setFile] = useState();
+  const [loading, setLoading] = useState(true);
+  const [fileName, setFileName] = useState("");
 
   
-  console.log("textEditor", fileId)
 
+  console.log("textEditor", fileId);
 
   const fetchFile = async () => {
-    console.log("fileId", fileId)
+    console.log("fileId", fileId);
     if (!fileId) return;
 
     try {
-      const res = await fetch(`/api/files/${fileId}`,{
-        method: 'GET',
+      const res = await fetch(`/api/files/${fileId}`, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
-        }
+        },
       });
-      console.log("Fetching File...")
+      console.log("Fetching File...");
 
       if (res.ok) {
-        console.log("res.ok for file fetching")
+        console.log("res.ok for file fetching");
         const data = await res.json();
 
-        setFile(data)
-        
-        console.log("file contents", data.content)
+        setFile(data);
 
+        console.log("file contents", data.content);
       } else {
-        throw new Error("Error fetching file for document")
+        throw new Error("Error fetching file for document");
       }
-
-
     } catch (error) {
-      console.error("Error fetching document", error)
-
+      console.error("Error fetching document", error);
     }
-  }
+  };
 
   useEffect(() => {
     if (fileId && loading) {
       fetchFile();
-      setLoading(false)
+      setLoading(false);
     }
-  }, [fileId, loading])
+  }, [fileId, loading]);
 
   const initEditor = () => {
     // Create editor
     const editor = new EditorJS({
       // ID for the editor dom
-      holder: 'editorjs',
+      holder: "editorjs",
       tools: {
-        header:{
+        header: {
           class: Header,
-          inlineToolbar: true
+          inlineToolbar: true,
         },
-        math:{
+        math: {
           class: MathTool,
-          config:{
-            katex:{ //Katex Rendering Configuration
+          config: {
+            katex: {
+              //Katex Rendering Configuration
               throwOnError: false, //Will not crash the entire site if katex fails to render
-            }
-          }
+            },
+          },
         },
       },
       data: {
-        blocks: file.content
+        blocks: file.content,
       },
       // Prevents editor border from extending down, creating unused space
       minHeight: 0,
@@ -104,56 +106,56 @@ const textEditor = () => {
         console.log(content);
       },
       // Initial text within the editor.
-      placeholder: 'Enter your text here!'
+      placeholder: "Enter your text here!",
     });
-  }
+  };
 
   // Ensures the editor loads once and destroys itself when it unmounts
   useEffect(() => {
-    if(ejInstance.current === null && file) {
+    if (ejInstance.current === null && file) {
       initEditor();
-      setFileName(file.name)
+      setFileName(file.name);
     }
 
     return () => {
       ejInstance.current?.destroy();
       ejInstance.current = null;
-    }
+    };
   }, [file]);
-  
+
   const insertMathBlock = () => {
-    ejInstance.current.blocks.insert('math', {});
-    setIsMathMode(true);  // Activate math mode
+    ejInstance.current.blocks.insert("math", {});
+    setIsMathMode(true); // Activate math mode
   };
 
   const exitMathMode = () => {
-    setIsMathMode(false);  // Deactivate math mode
+    setIsMathMode(false); // Deactivate math mode
   };
 
   const saveEditor = async () => {
     try {
-    const outputData = await ejInstance.current.save();
-    console.log('Article data: ', outputData);
-    const res = await fetch (`/api/files/${fileId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: file?.name,
-        content: outputData.blocks,
-      })
-    });
+      const outputData = await ejInstance.current.save();
+      console.log("Article data: ", outputData);
+      const res = await fetch(`/api/files/${fileId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: file?.name,
+          content: outputData.blocks,
+        }),
+      });
 
-    if (res.ok) {
-      const data = await res.json()
-      setFile(data)
-      console.log("updated content", data)
-    } else {
-      throw new Error("Error updating content")
-    }
+      if (res.ok) {
+        const data = await res.json();
+        setFile(data);
+        console.log("updated content", data);
+      } else {
+        throw new Error("Error updating content");
+      }
     } catch (error) {
-      console.log('Saving failed: ', error)
+      console.log("Saving failed: ", error);
     }
   };
 
@@ -166,116 +168,199 @@ const textEditor = () => {
         },
         body: JSON.stringify({
           name: fileName,
-          content: file.content 
-        })
+          content: file.content,
+        }),
       });
       if (res.ok) {
-        const data = await res.json()
-        setFile(data)
-        console.log("updated content", data)
+        const data = await res.json();
+        setFile(data);
+        console.log("updated content", data);
       } else {
-        throw new Error("Error updating name")
+        throw new Error("Error updating name");
       }
-
     } catch (error) {
-      console.error("error saving Name", error)
-
+      console.error("error saving Name", error);
     }
-
-  }
+  };
 
   const handleChange = (event) => {
-    setFileName(event.target.value)
-  }
+    setFileName(event.target.value);
+  };
 
   useEffect(() => {
     if (file) {
-      updateFileName()
+      updateFileName();
     }
-  }, [fileName])
-
-
-
-
+  }, [fileName]);
 
   useEffect(() => {
     if (file) {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [file])
+  }, [file]);
 
+  const exportToPDF = async () => {
+    try {
+      const outputData = await ejInstance.current.save();
+      const doc = new jsPDF();
+      let yPosition = 30;
+  
+      for (const block of outputData.blocks) {
+        if (block.type === "header") {
+          doc.setFontSize(14);
+          doc.setFont("helvetica", "bold");
+          doc.text(block.data.text, 10, yPosition);
+          yPosition += 10;
+        } else if (block.type === "paragraph") {
+          doc.setFontSize(12);
+          yPosition = renderStyledText(doc, block.data.text, yPosition);
+        } else if (block.type === "math") {
+          // Await renderMathBlock to ensure asynchronous rendering
+          yPosition = await renderMathBlock(doc, block.data.text, yPosition);
+        }
+        yPosition += 10;
+      }
+  
+      doc.save(`${fileName || "document"}.pdf`);
+    } catch (error) {
+      console.error("Error exporting to PDF", error);
+    }
+  };
+
+  const renderStyledText = (doc, text, yPosition) => {
+    // Split text by bold tags, identifying <b> tags
+    const segments = text.split(/(<b>|<\/b>)/);
+    let xPosition = 10;
+  
+    segments.forEach((segment) => {
+      if (segment === "<b>") {
+        doc.setFont("helvetica", "bold"); // Set font to bold
+      } else if (segment === "</b>") {
+        doc.setFont("helvetica", "normal"); // Reset font to normal
+      } else {
+        doc.text(segment, xPosition, yPosition);
+        xPosition += doc.getTextWidth(segment); // Adjust x-position for the next segment
+      }
+    });
+  
+    return yPosition + 10; // Return the updated y-position for the next line
+  };
+
+  const cleanLatex = (latex) => {
+    // Regex to match unsupported color functions like 'oklch()'
+    const colorRegex = /\\textcolor\{oklch\([^\}]+\)\}/g;
+  
+    // Replace all occurrences with a supported color
+    return latex.replace(colorRegex, (match) => {
+      // Replace with a generic color (e.g., 'red')
+      return match.replace("oklch", "red");
+    });
+  };
+
+  const renderMathBlock = async (doc, latex, yPosition) => {
+    const cleanLatexString = cleanLatex(latex);
+    // Create a temporary div for rendering LaTeX
+    const div = document.createElement("div");
+    div.style.position = "absolute";
+    div.style.left = "-9999px"; // Hide element offscreen
+    div.style.color = "#000000"; // Set color to black to avoid unsupported colors
+    div.style.backgroundColor = "#FFFFFF"; // Use a plain background
+  
+    document.body.appendChild(div);
+  
+    try {
+      const latexString = String(latex || ""); // Ensure latex is a string
+  
+      // Render the LaTeX to SVG
+      katex.render(latexString, div, {
+        throwOnError: false,
+        displayMode: true,
+      });
+  
+      // Convert SVG to canvas using html2canvas with a white background
+      const canvas = await html2canvas(div, { backgroundColor: "#FFFFFF" });
+      const imgData = canvas.toDataURL("image/png");
+  
+      // Calculate dimensions for the PDF
+      const imgWidth = 180;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+  
+      // Add image to the PDF
+      doc.addImage(imgData, "PNG", 10, yPosition, imgWidth, imgHeight);
+      yPosition += imgHeight;
+    } catch (error) {
+      console.error("Error rendering math block:", error);
+    } finally {
+      document.body.removeChild(div); // Clean up temporary div
+    }
+  
+    return yPosition;
+  };
+ 
 
   if (loading) {
     return (
-    <div>
-      <div id="editor-toolbar" className="">
-        <div id="tool" className="basis-1/10">
-          <button
-            onClick={file ? saveEditor: null}
-          >
-            <FontAwesomeIcon icon={faFloppyDisk} />
-          </button>
-        </div>
-        <div id="tool" className="basis-1/10">
-          {!isMathMode && (
-            <button
-              onClick={insertMathBlock}
-            >
-              <FontAwesomeIcon icon={faSquareRootVariable} />
+      <div className="flex">
+        <div id="editor-toolbar" className="">
+          <div id="tool" className="basis-1/10">
+            <button onClick={file ? saveEditor : null}>
+              <FontAwesomeIcon icon={faFloppyDisk} />
             </button>
-          )}
-          {isMathMode && (
-            <button
-              onClick={exitMathMode}
-            >
-              <FontAwesomeIcon icon={faX} />
-            </button>
-          )}
+          </div>
+          <div id="tool" className="basis-1/10">
+            {!isMathMode && (
+              <button onClick={insertMathBlock}>
+                <FontAwesomeIcon icon={faSquareRootVariable} />
+              </button>
+            )}
+            {isMathMode && (
+              <button onClick={exitMathMode}>
+                <FontAwesomeIcon icon={faX} />
+              </button>
+            )}
+          </div>
+          <div id="tool" className="basis-1/10">
+            <p>T2</p>
+          </div>
         </div>
-        <div id="tool" className="basis-1/10">
-          <p>T2</p>
+        <div id="filename" className="">
+          <form>
+            <input
+              className="filename-text"
+              onChange={(e) => {
+                setFileName(e);
+              }}
+              type="text"
+              name="filename"
+              value="Loading Document"
+            ></input>
+          </form>
         </div>
+
+        <div id="editorjs" className="rounded-lg"></div>
+
+        {isMathMode && <SymbolPicker />}
       </div>
-
-      <div id="filename" className="">
-          <form><input className="filename-text" onChange={(e) => {setFileName(e)}} type="text" name="filename" value="Loading Document" ></input></form>
-      </div>
-
-      <div id="editorjs" className="rounded-lg"></div>
-
-      {isMathMode && <SymbolPicker/>}
-    </div>
-
-    )
+    );
   }
 
-
-
-  
-
   // Displays the editor
-    return (
+  return (
     <div>
       <div id="editor-toolbar" className="">
         <div id="tool" className="basis-1/10">
-          <button
-            onClick={saveEditor}
-          >
+          <button onClick={saveEditor}>
             <FontAwesomeIcon icon={faFloppyDisk} />
           </button>
         </div>
         <div id="tool" className="basis-1/10">
           {!isMathMode && (
-            <button
-              onClick={insertMathBlock}
-            >
+            <button onClick={insertMathBlock}>
               <FontAwesomeIcon icon={faSquareRootVariable} />
             </button>
           )}
           {isMathMode && (
-            <button
-              onClick={exitMathMode}
-            >
+            <button onClick={exitMathMode}>
               <FontAwesomeIcon icon={faX} />
             </button>
           )}
@@ -283,17 +368,32 @@ const textEditor = () => {
         <div id="tool" className="basis-1/10">
           <p>T2</p>
         </div>
+        <div id="tool" className="basis-1/10">
+          <button onClick={exportToPDF} >
+            <FontAwesomeIcon icon={faFileExport} />
+          </button>
+        </div>
       </div>
 
       <div id="filename" className="">
-          <form><input className="filename-text"  onChange={handleChange} type="text" name="filename" value={fileName} ></input></form>
+        <form>
+          <input
+            className="filename-text"
+            onChange={handleChange}
+            type="text"
+            name="filename"
+            value={fileName}
+          ></input>
+        </form>
       </div>
 
       <div id="editorjs" className="rounded-lg"></div>
 
-      {isMathMode && <SymbolPicker/>}
+      {isMathMode && <SymbolPicker />}
+
+      
     </div>
-  )
-}
+  );
+};
 
 export default textEditor;
