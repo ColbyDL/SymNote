fc230cffc08d524a3c8722dfb326fe101fedb9fc
Colby Lalicker <colby.d.lalicker@gmail.com>
2024-11-18 02:27:28 -0500

    revamp profile screen

--- services/FileService.js
+++ services/FileService.js
@@ -1,10 +1,20 @@
 // services/FileService.js
 import File from '../models/file';
+import Folder from '../models/folder'
 import GenericService from './GenericService';
 
 class FileService extends GenericService {
     async createFile(data) {
         return await this.create(data);
+    }
+
+    async moveFile(fileId, oldFolderId, newFolderId) {
+
+        await Folder.findByIdAndUpdate(oldFolderId, { $pull: { files: fileId }});
+
+        await Folder.findByIdAndUpdate(newFolderId, { $push: { files: fileId }});
+
+        return await File.findByIdAndUpdate(fileId, { folderId: newFolderId }, { new: true });
     }
 }
 
